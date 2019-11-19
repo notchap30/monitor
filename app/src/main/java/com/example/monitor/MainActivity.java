5729683a12f4224071a5c5ae682648985d8ee280
notchap30 <imnotchap30@users.noreply.github.com>
2019-11-20 01:42:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/monitor/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.monitor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView botttomnav = findViewById(R.id.bottom_navigation);
+        botttomnav.setOnNavigationItemSelectedListener(NavListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener NavListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_fav:
+                            selectedFragment = new FavFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+    }
+
